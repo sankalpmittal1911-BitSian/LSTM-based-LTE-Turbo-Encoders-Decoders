9031b82a16c3cf92bcef4b4594013d1cc3d87962
Sankalp Mittal <36561982+sankalpmittal1911-BitSian@users.noreply.github.com>
2019-03-22 04:15:14 +0530

    Update x.py

--- x.py
+++ x.py
@@ -1,6 +1,6 @@
-def identity_block(input_tensor, kernel_size, filters, stage, block,
-                   use_bias=True, train_bn=True):
-    """The identity_block is the block that has no conv layer at shortcut
+def conv_block(input_tensor, kernel_size, filters, stage, block,
+               strides=(2, 2, 2), use_bias=True, train_bn=True):
+    """conv_block is the block that has a conv layer at shortcut
     # Arguments
         input_tensor: input tensor
         kernel_size: default 3, the kernel size of middle conv layer at main path
@@ -9,13 +9,15 @@ def identity_block(input_tensor, kernel_size, filters, stage, block,
         block: 'a','b'..., current block label, used for generating layer names
         use_bias: Boolean. To use or not use a bias in conv layers.
         train_bn: Boolean. Train or freeze Batch Norm layers
+    Note that from stage 3, the first conv layer at main path is with subsample=(2,2)
+    And the shortcut should have subsample=(2,2) as well
     """
     nb_filter1, nb_filter2, nb_filter3 = filters
     conv_name_base = 'res' + str(stage) + block + '_branch'
     bn_name_base = 'bn' + str(stage) + block + '_branch'
 
-    x = KL.Conv3D(nb_filter1, (1, 1, 1), name=conv_name_base + '2a',
-                  use_bias=use_bias)(input_tensor)
+    x = KL.Conv3D(nb_filter1, (1, 1, 1), strides=strides,
+                  name=conv_name_base + '2a', use_bias=use_bias)(input_tensor)
     x = BatchNorm(name=bn_name_base + '2a')(x, training=train_bn)
     x = KL.Activation('relu')(x)
 
@@ -24,10 +26,14 @@ def identity_block(input_tensor, kernel_size, filters, stage, block,
     x = BatchNorm(name=bn_name_base + '2b')(x, training=train_bn)
     x = KL.Activation('relu')(x)
 
-    x = KL.Conv3D(nb_filter3, (1, 1, 1), name=conv_name_base + '2c',
-                  use_bias=use_bias)(x)
+    x = KL.Conv3D(nb_filter3, (1, 1, 1), name=conv_name_base +
+                  '2c', use_bias=use_bias)(x)
     x = BatchNorm(name=bn_name_base + '2c')(x, training=train_bn)
 
-    x = KL.Add()([x, input_tensor])
+    shortcut = KL.Conv3D(nb_filter3, (1, 1, 1), strides=strides,
+                         name=conv_name_base + '1', use_bias=use_bias)(input_tensor)
+    shortcut = BatchNorm(name=bn_name_base + '1')(shortcut, training=train_bn)
+
+    x = KL.Add()([x, shortcut])
     x = KL.Activation('relu', name='res' + str(stage) + block + '_out')(x)
     return x
